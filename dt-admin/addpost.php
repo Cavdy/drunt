9cb3721cb984a9aac25312c82b8ac3034edd56fd
Cavdy <cavdyofficials@gmail.com>
2018-12-15 22:33:38 +0100

    added php functionality to add post

--- dt-admin/addpost.php
+++ dt-admin/addpost.php
@@ -1,8 +1,40 @@
 <?php 
     require_once('core/index.php');
-    include('includes/header.php'); 
-?>
 
+    $dateTime = strftime("%B-%d-%Y %H:%M:%S", time());
+
+  if(isset($_POST['submit'])) {
+    $postTitle = $drunt-> checkInput($_POST['posttitle']);
+    $postContent = $drunt-> checkInput($_POST['postbody']);
+    $postCategory = $drunt-> checkInput($_POST['category']);
+    $admin = "Cavdy";
+    $Image = $_FILES["imageselect"]['name'];
+    $ImageDir = "assets/upload/images".basename($Image);
+
+    if(empty($postTitle) || empty($postContent) || empty($postCategory)) {
+      $_SESSION['ErrorMessage'] = "All fields are required";
+      $drunt-> redirect("addpost.php");
+    } else if(strlen($postTitle) < 2 || strlen($postContent) < 2) {
+      $_SESSION['ErrorMessage'] = "Fields should not be less than 2 characters";
+      $drunt-> redirect("addpost.php");
+    } else {
+      global $connect;
+
+      $Query = "INSERT INTO dt_addpost(post_datetime,post_title,post_content,post_category, post_thumbnail, post_author) VALUES('$dateTime', '$postTitle', '$postContent', '$postCategory', '$Image', '$admin')";
+      $Execute = mysqli_query($connect,$Query);
+      move_uploaded_file($_FILES["imageselect"]["tmp_name"], $ImageDir);
+      if($Execute) {
+        $_SESSION['SuccessMessage'] = "Post created successfully";
+        $drunt-> redirect("addpost.php");
+      } else {
+        $_SESSION['ErrorMessage'] = "Failed to submit! Try again";
+        $drunt-> redirect("addpost.php");
+      }
+    }
+    mysqli_close($connect);
+  }
+?>
+    <?php include('includes/header.php'); ?>
     <div class="breadcrumbs">
         <div class="col-sm-4">
             <div class="page-header float-left">
@@ -26,34 +58,36 @@
 
     <div class="content mt-3">
         <div class="animated fadeIn">
-
-            <form action="" method="post" enctype="multipart/form-data">
+            
+            <form action="addpost.php" method="post" enctype="multipart/form-data">
 
             <div class="row">
                 <div class="col-lg-9">
-                
+                <?php echo ErrorMessage(); echo SuccessMessage(); ?>
+                        <!-- Post Title-->
                         <div class="card">
                             <div class="card-header">
                                 <strong class="card-title">Post Title</strong>
                             </div>
                             <div class="card-body">
                                 <div class="form-group">
-                                <input name="posttitle" type="text" id="company" placeholder="Post title" class="form-control"></div>
+                                <input name="posttitle" type="text" placeholder="Post title" class="form-control"></div>
                             </div>
                         </div>
+
+                        <!-- Post Content -->
                         <div class="card">
                             <div class="card-body">
                                 
-                                <div name="postbody" id="editor">
-
-                                </div>
+                                <textarea name="postbody" rows="15" type="text" id="editor" class="form-control"></textarea>
                                 
                             </div>
                         </div> <!-- .card -->
-
+                        
+                        <!-- Create Button -->
                         <div class="card">
                             <div class="card-body">
-                                <a href="addpost.php" class="btn btn-outline-primary btn-block"><i class="fa fa-plus"></i>&nbsp; Create Post</a>
+                                <button type="submit" name="submit" class="btn btn-outline-primary btn-block"><i class="fa fa-plus"></i>&nbsp; Create Post</button>
                             </div>
                         </div>
                     
--- dt-admin/includes/header.php
+++ dt-admin/includes/header.php
@@ -25,6 +25,23 @@
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
+    <style>
+        textarea:focus, 
+        textarea.form-control:focus, 
+        input.form-control:focus, 
+        input[type=text]:focus, 
+        input[type=password]:focus, 
+        input[type=email]:focus, 
+        input[type=number]:focus, 
+        [type=text].form-control:focus, 
+        [type=password].form-control:focus, 
+        [type=email].form-control:focus, 
+        [type=tel].form-control:focus, 
+        [contenteditable].form-control:focus {
+        box-shadow: inset 0 -1px 0 #ddd;
+        }
+    </style>
+
     </head>
 
 
